38189cc1a0c8c02eceb367335f27bbaa9847ee0b
Ashiqur Tusher <ashikurt77@gmail.com>
2019-11-23 13:09:32 +0600

    Updates

---
+++ precache-manifest.6d83101fd34ca18d95379228f0a829a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858ff0370f3ac8c1340ae84a2056426f",
+    "url": "/cool-image/index.html"
+  },
+  {
+    "revision": "28b89da86587bb11d6eb",
+    "url": "/cool-image/static/css/main.30ccbcc7.chunk.css"
+  },
+  {
+    "revision": "86f9c8f48b1f9e5836a3",
+    "url": "/cool-image/static/js/2.2f308f13.chunk.js"
+  },
+  {
+    "revision": "28b89da86587bb11d6eb",
+    "url": "/cool-image/static/js/main.81838251.chunk.js"
+  },
+  {
+    "revision": "598c5954611b3154bb12",
+    "url": "/cool-image/static/js/runtime-main.637562f8.js"
+  }
+]);
